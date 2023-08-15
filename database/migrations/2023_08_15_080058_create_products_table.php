697ae777dc562b394d94167a194288127a909ed8
Amirhossein <Amirmemool12@gmail.com>
2023-08-15 11:50:26 +0330

    :card_file_box: DB: update the migration of product

--- database/migrations/2023_08_15_080058_create_products_table.php
+++ database/migrations/2023_08_15_080058_create_products_table.php
@@ -11,11 +11,17 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->string('name');
+            $table->string('color');
+            $table->integer('code')->unique();
             $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
         });
     }
 
@@ -24,7 +30,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('products');
     }
