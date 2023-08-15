7d98b606b4f5edbdce469e6a49ac828b5c81698c
Amirhossein <Amirmemool12@gmail.com>
2023-08-15 11:51:00 +0330

    :card_file_box: DB: update the migration file of the producer

--- database/migrations/2023_08_15_080152_create_producers_table.php
+++ database/migrations/2023_08_15_080152_create_producers_table.php
@@ -11,11 +11,19 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('producers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('notional_code');
+            $table->string('city');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
@@ -24,7 +32,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('producers');
     }
