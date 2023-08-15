91f7a41e46905f0d7514201cc80f104ad1d18411
Amirhossein <Amirmemool12@gmail.com>
2023-08-15 11:59:35 +0330

    :card_file_box: DB:update company factory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ * @extends Factory<Company>
  */
 class CompanyFactory extends Factory
 {
@@ -14,10 +15,12 @@ class CompanyFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'country' => $this->faker->company(),
+            'number' => $this->faker->phoneNumber(),
         ];
     }
 }
